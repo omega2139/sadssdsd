1e7e059065578f11a0d979c892f2afb633d53378
omega2139 <76456596+omega2139@users.noreply.github.com>
2021-02-08 15:59:43 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function submit()
+{
+    var display_student_array=[];
+    for (var j=1; j<=4; j++)
+{
+    var name_of_the_student = document.getElementById("name_of_the_student_"+j).nodeValue;
+    console.log(name_of_the_student);
+    name_of_the_student_array.push(name_of_the_student);
+}
+
+console.log(name_of_the_student_array);
+
+var lenght_of_the_name_array = name_of_the_student_array.lenght_of_the_name_array
+console.log(lenght_of_name_of_students_array);
+for (var k =0; k , lenght_of_name_of_students_array; k++)
+{
+    display_student_array.push("<h4>name -"+ name_of_the_student_array[k] + "</h4");
+    console.log(display_student_array);
+}
+
+console.log(deisplay_student_array);
+document.getElementById("display-name_with_commas").innerHTML=display_student_array
+var remove_commas = display_student_array.join(" ");
+console.log(remove_commas);
+document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+document.getElementById("submit_button").style.display="none";
+document.getElementById("sort_button").style.display = "inline-block;"
+}
